2237eabb6df8bb06500631b924b8771a1461ce75
Sflores-g <sandra.flores.g@hotmail.com>
2024-12-11 20:39:46 -0500

    subiendo proyecto

---
+++ word_frequency.py
@@ -0,0 +1,11 @@
+dict = {}
+frase = input("Frase:")
+lista_palabras=frase.split(" ")
+for palabra in lista_palabras:
+	if palabra in dict:
+		dict[palabra]+=1
+	else:
+		dict[palabra]=1	
+
+for campo,valor in dict.items():
+	print (campo,"->",valor)
