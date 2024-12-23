2bbd5c4b9b3a06cd393de922a1913171deef9c58
Sflores-g <sandra.flores.g@hotmail.com>
2024-12-22 19:38:11 -0500

    Actualizacion de codigo

--- src/word_frequency.py
+++ src/word_frequency.py
@@ -1,11 +1,20 @@
-dict = {}
+lista = {}
 frase = input("Frase:")
 lista_palabras=frase.split(" ")
-for palabra in lista_palabras:
-	if palabra in dict:
-		dict[palabra]+=1
-	else:
-		dict[palabra]=1	
+caracteres = [":",",","@", "!", "."]
 
-for campo,valor in dict.items():
+for palabra in lista_palabras:
+	if palabra in caracteres:
+		continue
+	
+	if palabra in lista:
+		lista[palabra]+=1
+	else:
+		lista[palabra]=1	
+
+sorted(lista)
+
+for campo,valor in lista.items():
+	
 	print (campo,"->",valor)
+
